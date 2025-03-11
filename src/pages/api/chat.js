04f600b5df55d0df95d408560f3ec9e7b2a291f9
mek0707 <mek0987279538@hotmail.com>
2025-03-12 04:25:35 +0700

    Update chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -45,8 +45,8 @@
 
 import mongoose from 'mongoose';
 
-// const MONGODB_URI = process.env.MONGODB_URI || 'your_mongodb_connection_string';
-const MONGODB_URI = "mongodb+srv://wongsatorn0987279538:RkIPWb5QsSpyKI4A@cluster0.kknvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const MONGODB_URI = process.env.MONGODB_URI || 'your_mongodb_connection_string';
+// const MONGODB_URI = "mongodb+srv://wongsatorn0987279538:RkIPWb5QsSpyKI4A@cluster0.kknvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // เชื่อมต่อ MongoDB (ถ้ายังไม่ได้เชื่อม)
 if (!mongoose.connections[0].readyState) {
   mongoose.connect(MONGODB_URI, {
